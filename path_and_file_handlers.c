5d4bf2ba313883a0653ea31ecc498f1255fbe103
Maxwell Nana Forson <nanaforsonjnr@gmail.com>
2023-11-14 00:37:50 +0000

    Moved code logic to a separate file
    The two functions moved are 'handle_with_path()' and the
    'handle_file_as_input()'. It makes grouping them easier

---
+++ path_and_file_handlers.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+/**
+ * handle_with_path - handles commands when the PATH is set
+ * @path_list: a list of pathnames in the PATH variable
+ * @sub_command: the command to execute
+ *
+ * Return: the exit code of the child process, else -1 if the command is not in
+ * the PATH provided
+ */
+int handle_with_path(path_t *path_list, char **sub_command)
+{
+	char path[BUFF_SIZE];
+
+	while (path_list != NULL)
+	{
+		sprintf(path, "%s%s%s", path_list->pathname, "/", sub_command[0]);
+		if (access(path, X_OK) == 0)
+		{
+			return (execute_command(path, sub_command));
+		}
+		else if (access(sub_command[0], X_OK) == 0)
+		{
+			return (execute_command(sub_command[0], sub_command));
+		}
+		path_list = path_list->next;
+	}
+
+	return (-1);
+}
+
+/**
+ * handle_file_as_input - handles execution when a file is given as input on
+ * the command line (non-interactive mode)
+ * @filename: the name of file containing the commands
+ * @path_list: a list of pathnames in the PATH variable
+ *
+ * Return: 0, or the exit status of the just exited process
+ */
+int handle_file_as_input(char *filename, path_t *path_list)
+{
+	char *line = NULL;
+	size_t n = 0;
+	int n_read, fd, exit_code = 0;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "%s: 0: Can't open %s\n", _getenv("_"), filename);
+		return (CMD_NOT_FOUND);
+	}
+
+	n_read = _getline(&line, &n, fd);
+	close(fd);
+
+	if (n_read == -1)
+	{
+		return (-1); /* reading file failed */
+	}
+
+	if (n_read)
+		exit_code = parse_line(line, path_list);
+
+	multi_free("sp", line, &path_list);
+
+	return (exit_code);
+}
